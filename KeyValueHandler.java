4fc17d383cdcf01aeade1f7252f8bbd0cf2b23db
Ziyan Liu <z363liu@uwaterloo.ca>
2019-07-15 13:38:59 -0400

    update

--- KeyValueHandler.java
+++ KeyValueHandler.java
@@ -17,6 +17,8 @@ import org.apache.curator.retry.*;
 import org.apache.curator.framework.*;
 import org.apache.curator.framework.api.*;
 
+import com.google.common.util.concurrent.Striped;
+
 public class KeyValueHandler implements KeyValueService.Iface, CuratorWatcher{
 	private Map<String, String> myMap;
 	private CuratorFramework curClient;
@@ -26,8 +28,10 @@ public class KeyValueHandler implements KeyValueService.Iface, CuratorWatcher{
 	private static Logger log;
 	private String primarySocket;
 	private List<String> backupSockets;
-//	private ReentrantLock concurrencyLock = new ReentrantLock();
 	private final ReentrantReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+    private ConcurrentLinkedQueue<KeyValueService.Client> clientPool = new ConcurrentLinkedQueue<KeyValueService.Client>();
+    
+    private Striped<Semaphore> striped = Striped.semaphore(64,1);
 
 	public KeyValueHandler(String host, int port, CuratorFramework curClient, String zkNode) {
 		this.host = host;
@@ -54,11 +58,11 @@ public class KeyValueHandler implements KeyValueService.Iface, CuratorWatcher{
 			log.error("Something is wrong - get method shouldnt be called by backup node");
 			throw new org.apache.thrift.TException("Something is wrong - get method shouldnt be called by backup node");
 		}
-		
+		Semaphore mutex = striped.get(key);
 		try {
 			//acquire read lock, the thread will be blocked until the lock can be acquired
 			log.info("GET method called");
-			this.readWriteLock.readLock().lock();	
+            mutex.acquire();
 			String ret = myMap.get(key);
 			if (ret == null)
 				return "";
@@ -69,7 +73,7 @@ public class KeyValueHandler implements KeyValueService.Iface, CuratorWatcher{
 			e.printStackTrace();
 		}finally {
 			// release the read lock no matter what
-			this.readWriteLock.readLock().unlock();
+            mutex.release();
 		}
 		
 		// if everything goes well the execution flow should never reach here 
@@ -81,38 +85,28 @@ public class KeyValueHandler implements KeyValueService.Iface, CuratorWatcher{
 			log.error("Something is wrong - put method shouldnt be called by backup node");
 			throw new org.apache.thrift.TException("Something is wrong - put method shouldnt be called by backup node");
 		}
-		
+		Semaphore mutex = striped.get(key);
 		try {
 			//acquire write lock, the thread will be blocked until the lock can be acquired
 			log.info("PUT method called");
-			this.readWriteLock.writeLock().lock();
+
+            mutex.acquire();
 			this.myMap.put(key, value);
-			//log.info("Primary map content: " + this.getMyMap().toString());
-			
-			//start to update MyMap for all backup nodes
+
+			//start to update MyMap for backup node
 			for(String backupSocket: this.backupSockets) {
-				// establish RPC from primary to backup socket
-				String backupHost = backupSocket.split(":")[0];
-				int backupPort = Integer.parseInt(backupSocket.split(":")[1]);
-				TSocket tSocket = new TSocket(backupHost, backupPort);
-	            TTransport tTransport = new TFramedTransport(tSocket);
-	            tTransport.open();
-	            TProtocol tProtocol = new TBinaryProtocol(tTransport);
-	            
-	            // retrieve backup client
-	            KeyValueService.Client backupClient = new KeyValueService.Client(tProtocol);
-	            
-	            // start to synchronize the backup node with primary node
-	            log.info("Start updating data in backup node" + backupHost + ":" + backupPort + " because primary node has new changes.");
-	            backupClient.syncWithPrimary(key, value);
-	            log.info("Finished updating data in backup node" + backupHost + ":" + backupPort);
-	           tTransport.close();
+                KeyValueService.Client myClient = null;
+                while(myClient == null){
+                    myClient = this.clientPool.poll();
+                }
+                myClient.syncWithPrimary(key, value);
+                this.clientPool.add(myClient);
 			}
 		}catch(Exception e) {
 			log.error("PUT METHOD ERROR");
 			e.printStackTrace();
 		}finally {
-			this.readWriteLock.writeLock().unlock();
+            mutex.release();
 		}
 	}
 	
@@ -121,18 +115,18 @@ public class KeyValueHandler implements KeyValueService.Iface, CuratorWatcher{
 			log.error("Something is wrong - syncWithPrimary method shouldnt be called by primary node");
 			throw new org.apache.thrift.TException("Something is wrong - syncWithPrimary method shouldnt be called by primary node");
 		}
-		
+		Semaphore mutex = striped.get(key);
 		try {
 			//acquire write lock, the thread will be blocked until the lock can be acquired
 			log.info("Backup node is syncing with primary node");
-			this.readWriteLock.writeLock().lock();
+
+            mutex.acquire();
 			this.myMap.put(key, value);
-			//log.info("Backup map content: " + this.getMyMap().toString());
 		}catch(Exception e) {
 			log.error("SYNC METHOD ERROR");
 			e.printStackTrace();
 		}finally {
-			this.readWriteLock.writeLock().unlock();
+            mutex.release();
 		}
 	}
 	
@@ -184,6 +178,30 @@ public class KeyValueHandler implements KeyValueService.Iface, CuratorWatcher{
 		
 		return this.host.equals(primaryHost) && this.port == primaryPort;
 	}
+    
+    private ConcurrentLinkedQueue<KeyValueService.Client> populateClientPool() throws org.apache.thrift.TException{
+        ConcurrentLinkedQueue<KeyValueService.Client> queue = new ConcurrentLinkedQueue<KeyValueService.Client>();
+        
+        for(int i = 0; i < 64; i++){
+            // establish RPC from primary to backup socket
+            String backupSocket = this.backupSockets.get(0);
+            
+            String backupHost = backupSocket.split(":")[0];
+            int backupPort = Integer.parseInt(backupSocket.split(":")[1]);
+            TSocket tSocket = new TSocket(backupHost, backupPort);
+	        TTransport tTransport = new TFramedTransport(tSocket);
+	        tTransport.open();
+	        TProtocol tProtocol = new TBinaryProtocol(tTransport);
+	            
+	        // retrieve backup client
+	        KeyValueService.Client myClient = new KeyValueService.Client(tProtocol);
+            
+            // add to the queue
+            queue.add(myClient);
+        }
+        
+        return queue;
+    }
 	
 	@Override
 	synchronized public void process(WatchedEvent event) {
@@ -226,6 +244,12 @@ public class KeyValueHandler implements KeyValueService.Iface, CuratorWatcher{
 	            log.info("Finished replicating data from primary node to backup node" + this.host + ":" + this.port);
 	            tTransport.close();
 			}		
+            
+            // if the current node is primary node and there exists backup node, start to prepare backup clients
+            if(this.isPrimary() && this.backupSockets.size() > 0){
+                this.clientPool = populateClientPool();
+                log.info("Done populating client pool: " + this.clientPool.toString());
+            }
 		}catch(InterruptedException e) {
 			e.printStackTrace();
 		}catch(Exception e) {
